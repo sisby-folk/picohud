32458f8302c31f8f2f4e5cc1f4bfc0c4d53d9c09
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-05-18 12:09:43 +1000

    typo in comments

--- src/main/java/folk/sisby/picohud/PicoHudConfig.java
+++ src/main/java/folk/sisby/picohud/PicoHudConfig.java
@@ -4,7 +4,7 @@ import org.quiltmc.config.api.WrappedConfig;
 import org.quiltmc.config.api.annotations.Comment;
 
 public class PicoHudConfig extends WrappedConfig {
-	@Comment("Whether the show overlay key should act as a toggle, instead of a hold")
+	@Comment("Whether the 'show overlay' key should act as a toggle, instead of a hold")
 	public final Boolean useKeyToggle = false;
 	@Comment("Whether to show rounded coordinates")
 	public final Boolean showCoordinates = true;
@@ -12,6 +12,6 @@ public class PicoHudConfig extends WrappedConfig {
 	public final Boolean showDirectionCardinal = true;
 	@Comment("Whether to show an indicator (e.g. [=+]) that indicates how travelling forward will affect your X and Z coordinates")
 	public final Boolean showDirectionAxes = false;
-	@Comment("Whether to the current day and time, formatted using seasons if it's installed")
+	@Comment("Whether to show the current day and time, formatted using seasons if it's installed")
 	public final Boolean showDayTime = true;
 }
