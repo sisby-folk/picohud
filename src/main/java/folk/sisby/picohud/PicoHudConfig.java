5e9bc5ebd8406b2bb2a967987073fc8afd4baa02
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-05-18 12:07:14 +1000

    fix defaults

--- src/main/java/folk/sisby/picohud/PicoHudConfig.java
+++ src/main/java/folk/sisby/picohud/PicoHudConfig.java
@@ -7,11 +7,11 @@ public class PicoHudConfig extends WrappedConfig {
 	@Comment("Whether the show overlay key should act as a toggle, instead of a hold")
 	public final Boolean useKeyToggle = false;
 	@Comment("Whether to show rounded coordinates")
-	public final Boolean showCoordinates = false;
+	public final Boolean showCoordinates = true;
 	@Comment("Whether to show cardinal facing direction")
-	public final Boolean showDirectionCardinal = false;
+	public final Boolean showDirectionCardinal = true;
 	@Comment("Whether to show an indicator (e.g. [=+]) that indicates how travelling forward will affect your X and Z coordinates")
 	public final Boolean showDirectionAxes = false;
 	@Comment("Whether to the current day and time, formatted using seasons if it's installed")
-	public final Boolean showDayTime = false;
+	public final Boolean showDayTime = true;
 }
