28b29c43c8f61cc91550adb2424b30406bd856fa
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-09-22 12:53:32 +1000

    clean like hell, use kaleido

--- src/main/java/folk/sisby/picohud/PicoHudConfig.java
+++ src/main/java/folk/sisby/picohud/PicoHudConfig.java
@@ -1,7 +1,7 @@
 package folk.sisby.picohud;
 
-import org.quiltmc.config.api.WrappedConfig;
-import org.quiltmc.config.api.annotations.Comment;
+import folk.sisby.kaleido.api.WrappedConfig;
+import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
 
 public class PicoHudConfig extends WrappedConfig {
 	@Comment("Whether the 'show overlay' key should act as a toggle, instead of a hold")
